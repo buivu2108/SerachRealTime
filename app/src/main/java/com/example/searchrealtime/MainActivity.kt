24ad501627ac7fae63c17b181a4e71238cd88ceb
buivu2108 <93514655+buivu2108@users.noreply.github.com>
2022-02-22 10:17:12 +0700

    new commit

--- app/src/main/java/com/example/searchrealtime/MainActivity.kt
+++ app/src/main/java/com/example/searchrealtime/MainActivity.kt
@@ -8,12 +8,16 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.searchrealtime.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    val behaviorSubject = BehaviorSubject.create<String>()!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,38 +32,44 @@ class MainActivity : AppCompatActivity() {
         val numberArray = ArrayList<Int>()
         for (i in 1..100) {
             numberArray.add(i)
-
         }
 
-        Observable.create<ArrayList<Int>> {
-            binding.searchView.setOnQueryTextListener(object :
-                SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    return true
-                }
+        binding.searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
 
-                override fun onQueryTextChange(newText: String?): Boolean {
-                    numberAdapter.numberArray.clear()
-                    val list = ArrayList<Int>()
-                    for (item in numberArray) {
-                        if (item.toString().contains(newText!!)) {
-                            list.add(item)
-                        }
-                    }
-
-                    it.onNext(list)
-
-                    return false
-                }
-            })
-        }.debounce(2000,TimeUnit.MILLISECONDS).observeOn(AndroidSchedulers.mainThread()).subscribeOn(AndroidSchedulers.mainThread()).subscribe({
-            numberAdapter.addData(it)
-        },{
-            it.printStackTrace()
-        },{
-
+            override fun onQueryTextChange(newText: String?): Boolean {
+                numberAdapter.numberArray.clear()
+                behaviorSubject.onNext(newText!!)
+                return false
+            }
         })
+
+        behaviorSubject.debounce(2000, TimeUnit.MILLISECONDS).flatMap {
+            search(it, numberArray)
+        }.subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                numberAdapter.addData(it)
+            }, {
+                it.printStackTrace()
+            }, {
+
+            })
     }
 
+    private fun search(text: String, numberArray: ArrayList<Int>) : Observable<ArrayList<Int>>{
+        return Observable.create<ArrayList<Int>> {
+            Thread.sleep(2000)
+            val list = ArrayList<Int>()
+            for (item in numberArray) {
+                if (item.toString().contains(text)) {
+                    list.add(item)
+                }
+            }
+            it.onNext(list)
+        }
+    }
 
 }
